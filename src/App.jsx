e6e78317b368582f39e16e34045ca4c98b922c2e
Jokcy <jokcylou@hotmail.com>
2016-01-13 17:02:27 +0800

    finish step-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,44 @@
 import React, { Component } from 'react';
-//import {Router} from 'react-router';
-//import routes from './routes.jsx';
+//import fetch from 'whatwg-fetch';
+
+/**
+ * state:
+ * 组件内状态，一般用来存储组件状态
+ */
 
 
 class App extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'state',
+      server: 'no'
+    };
+  }
+
+  fetchData() {
+    fetch('/test', {
+      method: 'post'
+    })
+      .then((resp) => {
+        console.log(resp);
+        return resp.text();
+      })
+      .then(json => {
+        this.setState({server: json});
+      })
+  }
+
   render() {
     return (
 
-      <h1>This is a React Component</h1>
+      <div>
+        <h1>name is:<span style={{color: 'red'}}>{this.state.name}</span></h1>
+        <p>data from server: <span style={{color: 'blue'}}>{this.state.server}</span></p>
+        <p>input name: <input type="text" value={this.state.name} onChange={(e) => this.setState({name: e.target.value})} /></p>
+        <button onClick={() => this.fetchData()}>click to get the data</button>
+      </div>
 
     );
   }
